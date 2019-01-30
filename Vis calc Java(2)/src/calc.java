ee85adf6276f6b3d9c8fb557170774ee0281f0b9
ChaoticCross12 <40477982+ChaoticCross12@users.noreply.github.com>
2019-01-30 19:22:26 +0600

    Add files via upload
    Added 10 buttons

--- Vis calc Java(2)/src/calc.java
+++ Vis calc Java(2)/src/calc.java
@@ -13,6 +13,14 @@ public class calc
 	//creating buttons
 	private JButton b1;
 	private JButton b2;
+	private JButton b3;
+	private JButton b4;
+	private JButton b5;
+	private JButton b6;
+	private JButton b7;
+	private JButton b8;
+	private JButton b9;
+	private JButton b0;
 	
 	public calc() 
 	{	
@@ -30,12 +38,21 @@ public class calc
 		
 		//panel
 		p = new JPanel();
-		p.setBackground(Color.BLUE);
+		p.setBackground(Color.DARK_GRAY);
 
 		
 		//button
 		b1 = new JButton("1");
 		b2 = new JButton("2");
+		b3 = new JButton("3");
+		b4 = new JButton("4");
+		b5 = new JButton("5");
+		b6 = new JButton("6");
+		b7 = new JButton("7");
+		b8 = new JButton("8");
+		b9 = new JButton("9");
+		b0 = new JButton("0");
+		
 		
 		
 		//adding button to panel and frame
@@ -43,6 +60,30 @@ public class calc
 		f.add(p);
 		
 		p.add(b2);
+		f.add(p);
+		
+		p.add(b3);
+		f.add(p);
+		
+		p.add(b4);
+		f.add(p);
+		
+		p.add(b5);
+		f.add(p);
+		
+		p.add(b6);
+		f.add(p);
+		
+		p.add(b7);
+		f.add(p);
+		
+		p.add(b8);
+		f.add(p);
+		
+		p.add(b9);
+		f.add(p);
+		
+		p.add(b0);
 		f.add(p);
 	}
 	
