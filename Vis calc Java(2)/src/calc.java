b174ed6910ac4b2cd6082fa1309f0fc946d737cb
ChaoticCross12 <40477982+ChaoticCross12@users.noreply.github.com>
2019-01-31 22:50:33 +0600

    Added function buttons

--- Vis calc Java(2)/src/calc.java
+++ Vis calc Java(2)/src/calc.java
@@ -22,6 +22,13 @@ public class calc
 	private JButton b9;
 	private JButton b0;
 	
+	private JButton func1;
+	private JButton func2;
+	private JButton func3;
+	private JButton func4;
+	private JButton func5;
+	private JButton butres;
+	
 	public calc() 
 	{	
 		gui();
@@ -32,10 +39,12 @@ public class calc
 		//frame
 		f = new JFrame("Calculator");
 		f.setVisible(true);
-		f.setSize(500, 600);
+		f.setSize(370, 500);
 		f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		f.setResizable(false);
 		f.setLayout(null);
+		f.setBackground(Color.BLACK);
+		f.setFont(new Font("Times Romal", Font.BOLD, 0));
 
 		
 		//panel
@@ -75,10 +84,26 @@ public class calc
 		b0 = new JButton("0");
 		b0.setBounds(50, 370, 50, 50);
 		
+		func1 = new JButton("=");
+		func1.setBounds(120, 370, 50, 50);
+		
+		func2 = new JButton("+");
+		func2.setBounds(260, 230, 50, 50);
+		
+		func3 = new JButton("-");
+		func3.setBounds(260, 300, 50, 50);
+		
+		func4 = new JButton("x");
+		func4.setBounds(260, 370, 50, 50);
+		
+		func5 = new JButton("/");
+		func5.setBounds(190, 370, 50, 50);
+		
+		butres = new JButton("AC");
+		butres.setBounds(260, 160, 50, 50);
 		
 		//adding button to panel;
 		f.add(b1);
-		//p.add(b1);
 		f.add(b2);
 		f.add(b3);
 		f.add(b4);
@@ -88,7 +113,13 @@ public class calc
 		f.add(b8);
 		f.add(b9);	
 		f.add(b0);
-
+		
+		f.add(func1);
+		f.add(func2);
+		f.add(func3);
+		f.add(func4);
+		f.add(func5);
+		f.add(butres);
 	}
 	
 	
